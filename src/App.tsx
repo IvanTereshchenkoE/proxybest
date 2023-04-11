0e002afcbc4a4e506644bab8067181ba09176946
h8m3m0r3 <danilayumatov@yandex.ru>
2023-04-11 14:36:59 +0300

    auth default

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Main from './pages/main/main';
+import Header from './components/Header';
+import AuthPage from './pages/auth/authPage';
+import './App.css'
+import RegPage from './pages/auth/regPage';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <RegPage/>
+      <AuthPage />
+      <Header />
+      {/* <Main /> */}
     </div>
   );
 }
